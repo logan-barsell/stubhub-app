8a76e4d9c5d7552030f61df6c2b61f29b36aba22
Logan Barsell <loganjbars@gmail.com>
2024-07-22 19:14:39 -0700

    GCP config + basic routes + error handling

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,14 @@
+import { CustomError } from './custom-error';
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
+  reason = 'Error connecting to database';
+  constructor() {
+    super('Invalid Request.');
+    // * extending a built in class *
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+  serializeErrors() {
+    return [{ message: this.reason }];
+  }
+}
